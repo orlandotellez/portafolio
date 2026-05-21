842c6bb15a85eb8a10c26095b0e1fe4fda854f2f
orlandotellez <espadachinkinno@gmail.com>
2026-05-20 22:59:22 -0600

    feat: actualizar diseño del portafolio y cambiar componentes

--- src/data/redesSociales.js
+++ src/data/redesSociales.js
@@ -1,2 +1,2 @@
-export const github = "https://github.com/OrlandoTellez"
+export const github = "https://github.com/orlandotellez"
 export const linkedIn = "https://www.linkedin.com/in/orlando-gabriel-t%C3%A9llez"
