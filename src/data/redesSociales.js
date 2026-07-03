d2fc1fe7ee77f1517ef4a8700ab4cde2eabb3f29
orlandotellez <espadachinkinno@gmail.com>
2026-07-03 16:18:20 -0600

    refactor: reorganizar sidebar y agregar enlace de youtube
    - Mover email debajo del perfil
    - Quitar Open to work
    - Quitar ubicacion Managua
    - Cambiar iconos github/linkedin a SVG
    - Agregar youtube debajo de linkedin
    - Mover email al footer

--- src/data/redesSociales.js
+++ src/data/redesSociales.js
@@ -1,2 +1,3 @@
 export const github = "https://github.com/orlandotellez"
 export const linkedIn = "https://www.linkedin.com/in/orlando-gabriel-t%C3%A9llez"
+export const youtube = "https://www.youtube.com/@landodev"
